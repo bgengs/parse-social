e2c72f7aa2c8405b4ffd18d5285103c5d726833b
Bernie Gengel <berniegengel@gmail.com>
2017-12-24 11:13:55 -0500

    Update parse.php

--- config/parse.php
+++ config/parse.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-	'app_id' => 'ca55fdafb12b30271452fc46e5c00e33db90805ecd7e736291cd63ac2cc87e7c',
-	'master_key' => 'ylX9r7ErMCcUUvxG5cAOM9uQgF7NfnNikc1jrCzIfnw',
-	'server_url' => 'https://britsionary.org:1337',
-	'mount_path' => 'videniye'
+	'app_id' => '',
+	'master_key' => '',
+	'server_url' => '',
+	'mount_path' => ''
 ];
